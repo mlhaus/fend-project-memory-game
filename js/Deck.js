294e9acbefea3a356e266211279494cb99569f55
Marc Hauschildt <marc.hauschildt@gmail.com>
2018-06-23 18:10:18 -0500

    Created Deck class

---
+++ js/Deck.js
@@ -0,0 +1,15 @@
+let Deck = function() {
+  this.cards = [];
+  // Shuffle function from http://stackoverflow.com/a/2450976
+  this.shuffle = function(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  };
+};
